c1464b4cc1d99d20d0bfd79401b88732757e91b5
smileamaxdev <novoseltsev.as@phystech.edu>
2021-09-28 23:46:37 +0300

    first main

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include "figures/triangle.hpp"
+#include "scene/bsp_tree.hpp"
+#include <vector>
+#include <iostream>
+
+std::vector<rytg::Triangle> parseTriangles(){
+    size_t N = 0;
+    std::cin >> N;
+    std::vector<rytg::Triangle> result(N);
+    double x, y, z;
+    for(size_t i = 0; i < N; ++i){
+        rytg::Point p1, p2, p3;
+        std::cin >> x >> y >> z;
+        p1 = {x, y, z};
+        std::cin >> x >> y >> z;
+        p2 = {x, y, z};
+        std::cin >> x >> y >> z;
+        p3 = {x, y, z};
+        result[i] = rytg::Triangle(p1, p2, p3);
+    }
+    return result;
+}
+
+
+int main(){
+    auto v = parseTriangles();
+    rytg::BSPtree bsp(v);
+    return 0;
+}
