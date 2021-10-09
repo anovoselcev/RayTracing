8a987cbe6b0863e07bf3f1dfed7c48b4cd693749
smileamaxdev <novoseltsev.as@phystech.edu>
2021-10-09 13:25:55 +0300

    expanded interface of plane

--- geo/plane.cpp
+++ geo/plane.cpp
@@ -1,8 +1,13 @@
 #include "plane.hpp"
+#include "line.hpp"
 
 namespace rytg{
 
     Plane::Plane(Vector3D N, double d) noexcept : N_(N), d_(d){}
+
+    Plane::Plane(const Polygon& p) noexcept : Plane(p.getPlane()) {}
+
+    Plane::Plane(const Plane& p) noexcept : N_(p.N_), d_(p.d_){}
 
     Plane::Plane(const Triangle& t) noexcept{
         Vector3D edge1(t.getPoint(1), t.getPoint(0));
@@ -21,9 +26,13 @@ namespace rytg{
         return (N_.dot(v) + d_) == 0;
     }
 
+    bool Plane::isOnPlane(const Triangle& t) const noexcept{
+        return isOnPlane(t.getPoint(0)) && isOnPlane(t.getPoint(1)) && isOnPlane(t.getPoint(2));
+    }
+
     bool Plane::isAbove(Point p) const noexcept{
         double value = N_.dot(Vector3D(p)) + d_;
-        if(value >= 0) return true;
+        if(value > 0) return true;
         return false;
     }
 
@@ -31,8 +40,22 @@ namespace rytg{
         return isAbove(t.getPoint(0)) && isAbove(t.getPoint(1)) && isAbove(t.getPoint(2));
     }
 
-    bool Plane::isOnPlane(Point p) const noexcept{
-        return (N_.dot(Vector3D(p)) + d_) == 0;
+    bool Plane::isBelow(Point p) const noexcept{
+        double value = N_.dot(Vector3D(p)) + d_;
+        if(value < 0) return true;
+        return false;
+    }
+
+    bool Plane::isBelow(const Triangle& t) const noexcept{
+        return isBelow(t.getPoint(0)) && isBelow(t.getPoint(1)) && isBelow(t.getPoint(2));
+    }
+
+    bool Plane::isIntersection(const Triangle& t) const noexcept{
+        return !isAbove(t) && !isBelow(t);
+    }
+
+    double Plane::distance(Point p) const noexcept{
+        return N_.dot(Vector3D(p)) + d_;
     }
 
     Vector3D Plane::getNormal() const noexcept{
--- geo/plane.hpp
+++ geo/plane.hpp
@@ -1,11 +1,13 @@
 #ifndef PLANE_HPP
 #define PLANE_HPP
 
+//#include "line.hpp"
 #include "vector3d.hpp"
-#include "line.hpp"
 #include "../polygon/triangle.hpp"
 
 namespace rytg{
+
+    class Line;
 
     class Plane{
 
@@ -16,17 +18,29 @@ namespace rytg{
 
         Plane(const Triangle& t) noexcept;
 
+        Plane(const Polygon& p) noexcept;
+
+        Plane(const Plane& p) noexcept;
+
         Plane(Vector3D N, double d = 0) noexcept;
 
         bool isAbove(const Vector3D& v) const noexcept;
 
         bool isOnPlane(const Vector3D& v) const noexcept;
 
+        bool isOnPlane(const Triangle& v) const noexcept;
+
         bool isAbove(Point p) const noexcept;
 
         bool isAbove(const Triangle& t) const noexcept;
 
-        bool isOnPlane(Point p) const noexcept;
+        bool isBelow(Point p) const noexcept;
+
+        bool isBelow(const Triangle& t) const noexcept;
+
+        bool isIntersection(const Triangle& t) const noexcept;
+
+        double distance(Point p) const noexcept;
 
         Vector3D getNormal() const noexcept;
 
