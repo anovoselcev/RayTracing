eefbaea9ea850221829a4d544d6be98bd80342ba
smileamaxdev <novoseltsev.as@phystech.edu>
2021-10-09 13:31:23 +0300

    start to test

---
+++ tests/test_interception.hpp
@@ -0,0 +1,26 @@
+#include "../polygon/triangle.hpp"
+#include <cassert>
+#include <iostream>
+
+namespace test_rytg{
+
+void self(){
+    rytg::Triangle t1(rytg::Point{0.0, 0.0, 0.0}, rytg::Point{0.0, 0.0, 1.0}, rytg::Point{1.0, 0.0, 0.0});
+    auto t2 = t1;
+    //assert(t2.isIntersection(t1));
+    std::cout << "self done\n";
+}
+
+void normal(){
+    rytg::Triangle t1(rytg::Point{0.0, 0.0, 0.0}, rytg::Point{0.0, 0.0, 1.0}, rytg::Point{1.0, 0.0, 0.0});
+    rytg::Triangle t2(rytg::Point{0.0, 0.0, 0.0}, rytg::Point{0.0, 1.0, 1.0}, rytg::Point{1.0, 0.0, 0.0});
+    assert(t2.isIntersection(t1));
+    std::cout << "normal done\n";
+}
+
+void test_interception(){
+    self();
+    normal();
+}
+
+}
