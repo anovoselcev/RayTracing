b6e21efbd829373c9cfb86b23caafe463f3d7410
smileamaxdev <novoseltsev.as@phystech.edu>
2021-10-11 10:05:07 +0300

    fixed d dispatch intersection

--- polygon/triangle.cpp
+++ polygon/triangle.cpp
@@ -1,7 +1,9 @@
 #include "triangle.hpp"
+#include "chunk_triangle.hpp"
 #include "../geo/plane.hpp"
 #include "../geo/line.hpp"
-#include <iostream>
+
+#include <algorithm>
 
 namespace rytg{
 
@@ -20,28 +22,65 @@ namespace rytg{
         return Plane(*this);
     }
 
-    bool Triangle::isIntersection(const Polygon& p) const{
-        return p.isIntersection(*this);
+    bool Triangle::isIntersection(const Polygon* p) const{
+        if(const Triangle* t = dynamic_cast<const Triangle*>(p))
+            return t->isIntersection(this);
+        else if(const ChunkTriangle* ch = dynamic_cast<const ChunkTriangle*>(p))
+            return ch->isIntersection(this);
+        return false;
     }
 
-    bool Triangle::isIntersection(const Triangle& t) const noexcept{
+    bool Triangle::isIntersection(const Triangle* t) const noexcept{
         Plane p1(*this);
-        Plane p2(t);
+        Plane p2(*t);
 
         if(!p2.isIntersection(*this)) return false;
         
         if(p2.isOnPlane(*this)){
-            std::cout << "On plane\n";
+            //std::cout << "On plane\n";
         }
         Line L = p1.intersection(p2);
 
         auto t1 = L.intersection(*this, p2);
-        auto t2 = L.intersection(t, p1);
+        auto t2 = L.intersection(*t, p1);
 
         if(t1[0] <= t2[0] && t1[1] > t2[0]) return true;
         if(t2[0] <= t1[0] && t2[1] > t1[0]) return true;
 
         return false;
+    }
+
+    std::array<Polygon*, 2> Triangle::splitToChunks(Triangle* trig, const Plane& p){
+        auto line = p.intersection(trig->getPlane());
+        auto t = line.intersection(*trig, p);
+        std::vector<Point> above{line.getValue(t[0]), line.getValue(t[1])};
+        std::vector<Point> below(above);
+        for(uint8_t i = 0; i < 3; ++i){
+            if(p.isAbove(trig->getPoint(i))) above.push_back(trig->getPoint(i));
+            if(p.isBelow(trig->getPoint(i))) below.push_back(trig->getPoint(i));
+        }
+        auto last = std::unique(above.begin(), above.end());
+        above.erase(last, above.end());
+        last = std::unique(below.begin(), below.end());
+        below.erase(last, below.end());
+        if(above.size() <= 2) return {trig, nullptr};
+        if(below.size() <= 2) return {nullptr, trig};
+        auto trig_shared = std::make_shared<Triangle>(*trig);
+        delete trig;
+        Polygon* above_chunk = new ChunkTriangle(trig_shared, above);
+        Polygon* below_chunk = new ChunkTriangle(trig_shared, below);
+        return {below_chunk, above_chunk};
+    }
+
+    Triangle* Triangle::mergeChunks(const Polygon* lhs, const Polygon* rhs){
+        if(ChunkTriangle::isChunks(lhs, rhs)){
+
+            Triangle* par = dynamic_cast<const ChunkTriangle*>(lhs)->getParent();
+            delete lhs;
+            delete rhs;
+            return par;
+        }
+        return nullptr;
     }
 
 }
--- polygon/triangle.hpp
+++ polygon/triangle.hpp
@@ -14,17 +14,24 @@ namespace rytg{
 
         Triangle() = default;
 
+        ~Triangle() = default;
+
         Triangle(Point p1, Point p2, Point p3);
 
         Triangle(const Triangle& other) = default;
 
         Point getPoint(size_t v) const noexcept;
 
-        bool isIntersection(const Triangle& other) const noexcept;
+        bool isIntersection(const Triangle* other) const noexcept;
 
-        bool isIntersection(const Polygon& p) const override;
+        bool isIntersection(const Polygon* p) const override;
 
         Plane getPlane() const override;
+
+        static std::array<Polygon*, 2> splitToChunks(Triangle* t, const Plane& p);
+
+        static Triangle* mergeChunks(const Polygon* lhs, const Polygon* rhs);
+
     };
 }
 
