043f83a123ff84445336e8d3024679c9467c0c64
smileamaxdev <novoseltsev.as@phystech.edu>
2021-10-11 10:05:43 +0300

    make chunks owners of parent

--- polygon/chunk_triangle.cpp
+++ polygon/chunk_triangle.cpp
@@ -3,16 +3,28 @@
 
 namespace rytg{
 
-    ChunkTriangle::ChunkTriangle(Triangle* parent,
+    ChunkTriangle::ChunkTriangle(std::shared_ptr<Triangle> parent,
                                  const std::vector<Point>& points) noexcept :
                                  parent_(parent), p_(points){} 
 
-    bool ChunkTriangle::isIntersection(const Polygon& p) const{
+    bool ChunkTriangle::isIntersection(const Polygon* p) const{
         if(!parent_) return false;
         return parent_->isIntersection(p);
     }
 
     Plane ChunkTriangle::getPlane() const{
         if(parent_) return parent_->getPlane();
+        return Plane(Vector3D(), 0.0);
+    }
+
+    Triangle* ChunkTriangle::getParent() const noexcept{
+        return parent_.get();
+    }
+
+    bool ChunkTriangle::isChunks(const Polygon* lhs, const Polygon* rhs){
+        const ChunkTriangle* ch_lhs = dynamic_cast<const ChunkTriangle*>(lhs);
+        const ChunkTriangle* ch_rhs = dynamic_cast<const ChunkTriangle*>(rhs);
+        if(ch_lhs && ch_rhs && (ch_lhs->getParent() == ch_rhs->getParent())) return true;
+        return false;
     }
 }
--- polygon/chunk_triangle.hpp
+++ polygon/chunk_triangle.hpp
@@ -2,23 +2,30 @@
 #define CHUNK_TRIANGLE_HPP
 
 #include "triangle.hpp"
+#include <memory>
 #include <vector>
 
 namespace rytg{
     class ChunkTriangle : public Polygon{
         
-        Triangle* parent_ = nullptr;
+        std::shared_ptr<Triangle> parent_ = nullptr;
         std::vector<Point> p_;
 
         public:
 
         ChunkTriangle() = delete;
 
-        ChunkTriangle(Triangle* parent, const std::vector<Point>& points) noexcept;
+        ~ChunkTriangle() = default;
 
-        bool isIntersection(const Polygon& p) const;
+        ChunkTriangle(std::shared_ptr<Triangle> parent, const std::vector<Point>& points) noexcept;
+
+        bool isIntersection(const Polygon* p) const;
 
         Plane getPlane() const;
+
+        Triangle* getParent() const noexcept;
+
+        static bool isChunks(const Polygon* lhs, const Polygon* rhs);
 
     };
 }
