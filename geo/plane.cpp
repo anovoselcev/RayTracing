53095c01c3e0efb082a660217d1bd875440d3e52
smileamaxdev <novoseltsev.as@phystech.edu>
2021-10-11 10:02:22 +0300

    fix dispatch for planes

--- geo/plane.cpp
+++ geo/plane.cpp
@@ -1,5 +1,7 @@
 #include "plane.hpp"
 #include "line.hpp"
+#include "../polygon/chunk_triangle.hpp"
+#include <cmath>
 
 namespace rytg{
 
@@ -16,6 +18,10 @@ namespace rytg{
         d_ = - N_.dot(edge1);
     }
 
+    bool Plane::operator==(const Plane& p) const noexcept{
+        return N_.cross(p.N_).isNull() && std::fabs(d_) == std::fabs(p.d_);
+    }
+
     bool Plane::isAbove(const Vector3D& v) const noexcept{
         double value = N_.dot(v) + d_;
         if(value >= 0) return true;
@@ -30,6 +36,14 @@ namespace rytg{
         return isOnPlane(t.getPoint(0)) && isOnPlane(t.getPoint(1)) && isOnPlane(t.getPoint(2));
     }
 
+    bool Plane::isOnPlane(const Polygon& p) const noexcept{
+        if(const Triangle* t = dynamic_cast<const Triangle*>(&p))
+            return isOnPlane(*t);
+        else if(const ChunkTriangle* ct = dynamic_cast<const ChunkTriangle*>(&p))
+            return isOnPlane(*(ct->getParent()));
+        return false;
+    }
+
     bool Plane::isAbove(Point p) const noexcept{
         double value = N_.dot(Vector3D(p)) + d_;
         if(value > 0) return true;
@@ -38,6 +52,14 @@ namespace rytg{
 
     bool Plane::isAbove(const Triangle& t) const noexcept{
         return isAbove(t.getPoint(0)) && isAbove(t.getPoint(1)) && isAbove(t.getPoint(2));
+    }
+
+    bool Plane::isAbove(const Polygon& p) const noexcept{
+        if(const Triangle* t = dynamic_cast<const Triangle*>(&p))
+            return isAbove(*t);
+        else if(const ChunkTriangle* ct = dynamic_cast<const ChunkTriangle*>(&p))
+            return isAbove(*ct);
+        return false;
     }
 
     bool Plane::isBelow(Point p) const noexcept{
@@ -50,8 +72,24 @@ namespace rytg{
         return isBelow(t.getPoint(0)) && isBelow(t.getPoint(1)) && isBelow(t.getPoint(2));
     }
 
+    bool Plane::isBelow(const Polygon& p) const noexcept{
+        if(const Triangle* t = dynamic_cast<const Triangle*>(&p))
+            return isBelow(*t);
+        else if(const ChunkTriangle* ct = dynamic_cast<const ChunkTriangle*>(&p))
+            return isBelow(*ct);
+        return false;
+    }
+
     bool Plane::isIntersection(const Triangle& t) const noexcept{
         return !isAbove(t) && !isBelow(t);
+    }
+
+    bool Plane::isIntersection(const Polygon& p) const noexcept{
+        if(const Triangle* t = dynamic_cast<const Triangle*>(&p))
+            return isIntersection(*t);
+        else if(const ChunkTriangle* ct = dynamic_cast<const ChunkTriangle*>(&p))
+            return isIntersection(*ct);
+        return false;
     }
 
     double Plane::distance(Point p) const noexcept{
--- geo/plane.hpp
+++ geo/plane.hpp
@@ -24,27 +24,37 @@ namespace rytg{
 
         Plane(Vector3D N, double d = 0) noexcept;
 
-        bool isAbove(const Vector3D& v) const noexcept;
-
         bool isOnPlane(const Vector3D& v) const noexcept;
 
         bool isOnPlane(const Triangle& v) const noexcept;
+
+        bool isOnPlane(const Polygon& p) const noexcept;
+
+        bool isAbove(const Vector3D& v) const noexcept;
 
         bool isAbove(Point p) const noexcept;
 
         bool isAbove(const Triangle& t) const noexcept;
 
+        bool isAbove(const Polygon& t) const noexcept;
+
         bool isBelow(Point p) const noexcept;
 
         bool isBelow(const Triangle& t) const noexcept;
 
+        bool isBelow(const Polygon& t) const noexcept;
+
         bool isIntersection(const Triangle& t) const noexcept;
+
+        bool isIntersection(const Polygon& t) const noexcept;
 
         double distance(Point p) const noexcept;
 
         Vector3D getNormal() const noexcept;
 
         double getConstant() const noexcept;
+
+        bool operator==(const Plane& p) const noexcept;
 
         Line intersection(const Plane& p) const noexcept;
 
