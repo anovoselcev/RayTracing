6fe1f696f7bf9a3ae0a477fe61881e83fe75bab3
smileamaxdev <novoseltsev.as@phystech.edu>
2021-11-05 10:53:14 +0300

    structed tests

--- tests/tests.hpp
+++ tests/tests.hpp
@@ -2,17 +2,23 @@
 #define TESTS_HPP
 
 #include "tests/test_bsp.hpp"
-#include "tests/test_e2e.hpp"
 #include "tests/test_interception.hpp"
 #include "tests/test_vector.hpp"
 
 namespace test_rytg{
-void tests(){
+
+void tests_unite(){
     test_vector();
     test_interception();
     test_bsp();
+}
+#ifdef E2ETEST
+#include "tests/test_e2e.hpp"
+
+void test_standalone(){
     test_e2e();
 }
+#endif
 }
 
 #endif
